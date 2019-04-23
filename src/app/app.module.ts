2118cbf24cf2e77263af645c4f2045de294634f8
thienhoang563 <thienhoang563@gmail.com>
2019-04-23 09:49:55 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { IconsComponent } from './icons/icons.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { MapsComponent } from './maps/maps.component';
+import { NotificationsComponent } from './notifications/notifications.component';
+import { TableListComponent } from './table-list/table-list.component';
+import { TypographyComponent } from './typography/typography.component';
+import { UpgradeComponent } from './upgrade/upgrade.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    IconsComponent,
+    AdminLayoutComponent,
+    MapsComponent,
+    NotificationsComponent,
+    TableListComponent,
+    TypographyComponent,
+    UpgradeComponent,
+    UserProfileComponent,
+    NavbarComponent,
+    FooterComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
